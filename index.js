64126b1fc5ba1531c0c80529e328b7699c8f68e6
Pavel Grishutenko <grishutenkopp@gmail.com>
2021-10-21 21:50:39 +0300

    update script

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 80
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || "8080";
+app.set("port", port);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!')
+  })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port, () => console.log(`Server running on localhost:${port}`));
